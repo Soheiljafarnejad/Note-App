63fef09250aeb378da574ee61e1440f748271680
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-02-09 16:06:00 +0330

    added backdrop and colors section

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 const darkMode = [...document.querySelectorAll(".toggler")];
-darkMode.forEach((item)=>{
-    item.addEventListener("click", () => {
-      document.getElementById("root").classList.toggle("dark--mode");
-    });
-})
+darkMode.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.getElementById("root").classList.toggle("dark--mode");
+  });
+});
+
+const backdrop = document.querySelector(".backdrop");
+const btnCancel = document.querySelector(".btn--cancel");
+
+const addNote = [...document.querySelectorAll(".add--note")];
+addNote.forEach((item) => {
+  item.addEventListener("click", () => {
+    backdrop.style.display = "flex";
+  });
+});
+
+btnCancel.addEventListener("click", () => {
+  backdrop.style.display = "none";
+});
