d7899f31590cc5964d1b309034a0d020cbe2158a
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-02-09 17:01:43 +0330

    debug

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// darkMode
 const darkMode = [...document.querySelectorAll(".toggler")];
 darkMode.forEach((item) => {
   item.addEventListener("click", () => {
@@ -5,9 +6,9 @@ darkMode.forEach((item) => {
   });
 });
 
+// backdrop
 const backdrop = document.querySelector(".backdrop");
 const btnCancel = document.querySelector(".btn--cancel");
-
 const addNote = [...document.querySelectorAll(".add--note")];
 addNote.forEach((item) => {
   item.addEventListener("click", () => {
@@ -18,3 +19,4 @@ addNote.forEach((item) => {
 btnCancel.addEventListener("click", () => {
   backdrop.style.display = "none";
 });
+
